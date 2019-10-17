413e1c329fcc6196e1d509ceabbc8036a76c197d
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-10-17 19:36:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each(){|project|
     title = project.css("h2.bbcard_name strong a").text
-    projects
+    projects[title]
   }
 end
 
